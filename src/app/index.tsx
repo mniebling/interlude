078e7f20be9427ebe37d93b090cee11081bc9319
Mike Niebling <michael.niebling@gmail.com>
2023-07-07 22:18:09 -0500

    Test Vite deploy instead of NextJS

---
+++ src/app/index.tsx
@@ -0,0 +1,10 @@
+
+import { createRoot } from 'react-dom/client'
+
+
+const container = document.getElementById('root') as HTMLDivElement
+const root = createRoot(container)
+
+root.render(
+	<h1>Hello, world!</h1>
+)
---
+++ vite.config.ts
@@ -0,0 +1,22 @@
+import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+
+
+export default defineConfig(({ command, mode }) => ({
+	build: {
+		outDir: '../../dist', // this is relative to root
+	},
+	plugins: [
+		react(),
+	],
+	resolve: {
+		alias: {
+			'@': resolve('./src'),
+		},
+	},
+	root: './src/app',
+	server: {
+		hmr: false,
+	},
+}))
